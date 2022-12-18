2b8a7a2d9d91c05c9351df4aa99b7059f3d19fe5
Abhishek-76 <avinashgautam8923933406@gmail.com>
2022-12-18 19:22:42 +0530

    location3

--- src/components/CurrentLocation.js
+++ src/components/CurrentLocation.js
@@ -3,6 +3,7 @@ import api from "./apiKeys";
 
 const CurrentLocation = () => {
   const [query, setQuery] = useState("");
+  const [error, setError] = useState("");
   const [weather, setWeather] = useState("");
   const search = (evt) => {
     if (evt.key === "Enter") {
@@ -142,50 +143,61 @@ const CurrentLocation = () => {
     };
     render() {
       if (this.state.temperatureC) {
-        return <></>;
+        return (
+          <>
+            <div className="title">
+              <h2>{this.state.city}</h2>
+              <h3>{this.state.country}</h3>
+            </div>
+            <div className="mb-icon">
+              {""}
+              <ReactAnimatedWeather icon={this.state.icon} />
+            </div>
+          </>
+        );
       }
     }
   }
-  return (
-    <div
-      className={
-        typeof weather.main != "undefined"
-          ? weather.main.temp > 20
-            ? "app warm"
-            : "app"
-          : "app"
-      }
-    >
-      <main>
-        <div className="search-box">
-          <input
-            type="text"
-            className="search-bar  "
-            placeholder="Search..."
-            onChange={(e) => setQuery(e.target.value)}
-            value={query}
-            onKeyPress={search}
-          />
-        </div>
-        {typeof weather.main != "undefined" ? (
-          <div>
-            <div className="location-box">
-              <div className="location">
-                {weather.name},{weather.sys.country}
-              </div>
-              <div className="date">{dateBuilder(new Date())}</div>
-            </div>
-            <div className="weather-box">
-              <div className="temp"> {Math.round(weather.main.temp)} °C</div>
-              <div className="weather">{weather.weather[0].main}</div>
-            </div>
-          </div>
-        ) : (
-          ""
-        )}
-      </main>
-    </div>
-  );
+  // return (
+  //   <div
+  //     className={
+  //       typeof weather.main != "undefined"
+  //         ? weather.main.temp > 20
+  //           ? "app warm"
+  //           : "app"
+  //         : "app"
+  //     }
+  //   >
+  //     <main>
+  //       <div className="search-box">
+  //         <input
+  //           type="text"
+  //           className="search-bar  "
+  //           placeholder="Search..."
+  //           onChange={(e) => setQuery(e.target.value)}
+  //           value={query}
+  //           onKeyPress={search}
+  //         />
+  //       </div>
+  //       {typeof weather.main != "undefined" ? (
+  //         <div>
+  //           <div className="location-box">
+  //             <div className="location">
+  //               {weather.name},{weather.sys.country}
+  //             </div>
+  //             <div className="date">{dateBuilder(new Date())}</div>
+  //           </div>
+  //           <div className="weather-box">
+  //             <div className="temp"> {Math.round(weather.main.temp)} °C</div>
+  //             <div className="weather">{weather.weather[0].main}</div>
+  //           </div>
+  //         </div>
+  //       ) : (
+  //         ""
+  //       )}
+  //     </main>
+  //   </div>
+  // );
 };
 
 export default CurrentLocation;
